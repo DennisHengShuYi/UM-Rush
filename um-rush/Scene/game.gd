dacfecd3e5346bae6276b496af7bc7c0b6e232bc
den51 <den519000@gmail.com>
2026-05-19 22:38:20 +0800

    Spawn obstacle and layered background

--- um-rush/Scene/game.gd
+++ um-rush/Scene/game.gd
@@ -1,30 +1,22 @@
 extends Node2D
 
-@onready var parallax: ParallaxBackground = $ParallaxBackground
-
 var desk_scene = preload("res://Scene/obstacle.tscn")
 var spawn_timer = 0.0
 var spawn_interval = 2.0
-var scroll_speed = 200.0
 
-func _ready() -> void:
-	print("game.gd loaded")
-	print("parallax node: ", parallax)
+@onready var player = $Player
 
 func _process(delta: float) -> void:
+	# Only count timer when player is moving right
 	if Input.is_action_pressed("ui_right"):
-		print("RIGHT")
-		parallax.scroll_offset.x -= scroll_speed * delta
-	elif Input.is_action_pressed("ui_left"):
-		print("LEFT")
-		parallax.scroll_offset.x += scroll_speed * delta
-
-	spawn_timer += delta
-	if spawn_timer >= spawn_interval:
-		spawn_timer = 0.0
-		spawn_desk()
+		spawn_timer += delta
+		if spawn_timer >= spawn_interval:
+			spawn_timer = 0.0
+			spawn_desk()
 
 func spawn_desk():
 	var desk = desk_scene.instantiate()
 	add_child(desk)
-	desk.position = Vector2(1100, 530)
+	# Changed Y to match ground level
+	desk.position = Vector2(player.position.x + 800, -68)
+	print("Desk spawned at: ", desk.position)
--- um-rush/Scene/obstacle.gd
+++ um-rush/Scene/obstacle.gd
@@ -1,1 +1,12 @@
 extends StaticBody2D
+
+func _process(delta: float) -> void:
+	var camera = get_viewport().get_camera_2d()
+
+	if camera == null:
+		return
+
+	var camera_x = camera.get_screen_center_position().x
+
+	if position.x < camera_x - 1000:
+		queue_free()
